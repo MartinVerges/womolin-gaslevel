d7078b70ebb8a4b40cda94c539f8993c940e61a9
Martin Verges <martin.verges@croit.io>
2023-02-06 20:48:43 +0100

    add manifest generator to the build process

---
+++ tools/webinstaller-manifest-generator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import subprocess
+import argparse
+import json
+import csv
+import os
+
+build_version = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True).stdout.strip().strip('"')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--project', help="Name of the Project",
+                    action='store', metavar='<name>', required=True)
+parser.add_argument('-b', '--bucket', help="Name of the S3 Bucket where the files are located",
+                    action='store', metavar='<bucket>', default='webinstaller')
+parser.add_argument('-s', '--url', help="Base URL of the file storage",
+                    action='store', metavar='<url>',
+                    default='https://s3.womolin.de')
+parser.add_argument('-f', '--file', help="Partition layout csv file",
+                    action='store', metavar='partitions.csv', required=True)
+parser.add_argument('-v', '--version', help="Version of that build",
+                    action='store', metavar='<version>', default=build_version)
+parser.add_argument('-o', '--outfile', help="Filename of the output manifest file",
+                    action='store', metavar='<filename>', default="manifest.json")
+                
+args = vars(parser.parse_args())
+
+prefix = '%s/%s/%s/' % (args['url'], args['bucket'], args['project'])
+
+data = [
+    { "path": prefix + "bootloader_dio_80m.bin", "offset": 4096 },
+    { "path": prefix + "partitions.bin", "offset": 32768 },
+    { "path": prefix + "boot_app0.bin", "offset": 57344 },
+]
+
+with open(args['file']) as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',', skipinitialspace=True)
+    for row in reader:
+        row['OffsetDec'] = int(row['Offset'], base=16)
+        if row['SubType'].startswith('ota_'):
+            row['file'] = prefix + 'firmware.bin'
+        if row['SubType'].startswith('spiffs'):
+            row['file'] = prefix + 'littlefs.bin'
+        
+        if 'file' in row:
+            data.append({ "path": row['file'], "offset": row['OffsetDec'] })
+
+with open(os.path.dirname(__file__) + '/webinstaller-manifest-template.json') as user_file:
+  manifest = json.load(user_file)
+  manifest['name'] = args['project']
+  manifest['version'] = args['version']
+  manifest['builds'][0]['parts'] = data
+  # print(json.dumps(manifest, indent=2))
+
+  with open(args['outfile'], "w") as out:
+    print("Write manifest to %s" % args['outfile'])
+    json.dump(manifest, out, indent=2)
